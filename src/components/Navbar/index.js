e1175dda322658f748038d71162ad8a147c86f90
yusuplusminus <yoesoep17@gmail.com>
2022-10-12 13:32:48 +0700

    1.01.00

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Nav, NavbarContainer, NavLogo} from './NavbarElements';
+
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>dolla</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>About</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
