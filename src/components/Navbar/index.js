1f3659edc53073371b95fe555c3e840b71db0fdc
yusuplusminus <yoesoep17@gmail.com>
2022-10-19 13:52:00 +0700

    1.03.00

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,25 +1,47 @@
-import React from 'react'
-import {Nav, NavbarContainer, NavLogo} from './NavbarElements';
-
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink
+} from "./NavbarElements";
 
 const Navbar = () => {
-    return (
-        <>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo to='/'>dolla</NavLogo>
-                    <MobileIcon>
-                        <FaBars />
-                    </MobileIcon>
-                    <NavMenu>
-                        <NavItem>
-                            <NavLinks to='about'>About</NavLinks>
-                        </NavItem>
-                    </NavMenu>
-                </NavbarContainer>
-            </Nav>
-        </>
-    );
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">dolla</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="project">Project</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
 };
 
 export default Navbar;
