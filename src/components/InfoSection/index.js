834c664c7274212af12095250d187505cc6371f5
yusupaara <yoesoep17@gmail.com>
2022-10-23 01:01:53 +0700

    Halfway

---
+++ src/components/InfoSection/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Button } from "react-scroll";
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  Column2,
+  ImgWrap,
+  Img,
+} from "./InfoElements";
+
+function InfoSection() {
+  return (
+    <>
+      <InfoContainer>
+        <InfoWrapper>
+          <InfoRow>
+            <Column1>
+              <TextWrapper>
+                <TopLine>Topline</TopLine>
+                <Heading>Heading</Heading>
+                <Subtitle>Subtitle</Subtitle>
+                <BtnWrap>
+                  <Button to="home">Button</Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  );
+}
+
+export default InfoSection;
